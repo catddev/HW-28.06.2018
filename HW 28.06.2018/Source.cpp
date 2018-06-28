ac8403a262badb5ddd2fe025e21c2b95823fad38
dinara0708 <aikenova01@gmail.com>
2018-06-29 01:02:27 +0600

    -

--- HW 28.06.2018/Source.cpp
+++ HW 28.06.2018/Source.cpp
@@ -17,6 +17,10 @@ int main()
 
 	int tn = 0;
 
+
+	cout << "Введите число 1-6 для вывода соответствующей фигуры: " << endl << "1 - в, 2 - г, 3 - д, 4 - е, 5 - ж, 6 - з" << endl;
+	cout << "Либо переходите к заданиям 7-9" << endl << endl;
+
 start:
 
 	cout << "Задача № ";
@@ -24,54 +28,195 @@ start:
 
 	switch (tn)
 	{
+		// Вывести на экран фигуры, заполненные звездочками. Диалог с пользователем реализовать при помощи меню.
+
 	case 1:
 	{
-
+		int n = 9;
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				if (i <= j && (i + j <= n - 1)) cout << "*";
+				else cout << " ";
+			}
+			cout << endl;
+		}
 	}
 	break;
 	case 2:
 	{
-
+		int n = 9;
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				if (i >= j && (i + j >= n - 1)) cout << "*";
+				else cout << " ";
+			}
+			cout << endl;
+		}
+		cout << endl;
 	}
 	break;
 	case 3:
 	{
-
+		int n = 9;
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				if ((i <= j && (i + j <= n - 1)) || (i >= j && (i + j >= n - 1))) cout << "*";
+				else cout << " ";
+			}
+			cout << endl;
+		}
+		cout << endl;
 	}
 	break;
 	case 4:
 	{
-
+		int n = 9;
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				if ((i <= j && (i + j >= n - 1)) || (i >= j && (i + j <= n - 1))) cout << "*";
+				else cout << " ";
+			}
+			cout << endl;
+		}
+		cout << endl;
 	}
 	break;
 	case 5:
 	{
-
+		int n = 9;
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				if (i >= j && (i + j <= n - 1)) cout << "*";
+				else cout << " ";
+			}
+			cout << endl;
+		}
+		cout << endl;
 	}
 	break;
 	case 6:
 	{
+		int n = 9;
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				if (i <= j && (i + j >= n - 1)) cout << "*";
+				else cout << " ";
+			}
+			cout << endl;
+		}
+		cout << endl;
+	}
+	break;
+	case 10:
+	{
+		int n = 9, m = 9;
+		for (int i = 0; i < n; i++)
+		{
+			for (int i = 0; i < m; i++)
+			{
 
+				for (int j = 0; j < n; j++)
+				{
+					if (i >= j && (i + j >= n - 1)) cout << "*";
+					else cout << " ";
+
+					if (i <= j && (i + j <= m - 1)) cout << "*";
+					else cout << " ";
+				}
+				cout << endl;
+			}
+		}
 	}
 	break;
 	case 7:
 	{
+		// Вывести на экран ромб из зведочек
+
+		int n = 9;
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				if (i >= j && (i + j >= n - 1)) cout << "*";
+				else cout << " ";
+			}
+			cout << endl;
+		}
+		int m = 9;
+		for (int i = 0; i < m; i++)
+		{
+			for (int j = 0; j < m; j++)
+			{
+				if (i <= j && (i + j <= m- 1)) cout << "*";
+				else cout << " ";
+			}
+			cout << endl;
+		}
 
 	}
 	break;
 	case 8:
 	{
+		// Вывести на экран заданное количество чисел, кратных заданному
 
+		int n;
+		int k;
+		cout << "Введите число, которому должны быть кратны запрашиваемые числа" << endl << "Сколько чисел, кратных заданному вывести?" << endl;
+		cin >> n >> k;
+		int x = 0;
+		int number = 1;
+
+		while(x!=k)
+		{
+			if (number % n == 0)
+			{
+				cout << number << "\t";
+				x++;
+			}
+			number++;
+		}
+		cout << endl << endl;
 	}
 	break;
 	case 9:
 	{
+		// Для двух чисел определить наименьшее общее кратное
 
-	}
-	break;
-	case 10:
-	{
+		int a;
+		int b;
+		cin >> a >> b;
+		
+		if (a > b)
+		{
+			int tmp = a;
+			a = b;
+			b = tmp;
+		}
 
+		int i = 2;
+		while (a >= i)
+		{
+			if (a%i == 0 && b%i == 0)
+			{
+				cout << "Наименьшее общее кратное: " << i << endl;
+				break;
+			}
+			i++;
+		}
+		if (i > a) cout << "Числа не имеют НОК" << endl;
+		cout << endl;
 	}
 	break;
 	default:
